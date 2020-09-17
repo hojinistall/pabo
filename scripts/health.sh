ef09be0453dc13e9e5228dc64e3d6bfc99d62a1c
김호진 <>
2020-09-17 15:30:06 +0900

    switch deployment & health  4

--- scripts/health.sh
+++ scripts/health.sh
@@ -18,7 +18,7 @@ do
   RESPONSE=$(curl -s http://loclhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo  ${RESPONSE} | grep 'real' | wc -l)
 
-  if [ ${UP_CONT} -ge 1 ]
+  if [ ${UP_COUNT} -ge 1 ]
   then #  "real"문자열이 있는지 검증
     echo "> Health Check 성공"
     switch_proxy
@@ -28,7 +28,7 @@ do
     echo "> Health Check : ${RESPONSE}"
   fi
 
-  if [ ${RETRY_CONT} -eq 10 ]
+  if [ ${RETRY_COUNT} -eq 10 ]
   then
     echo "> Health Check  실패"
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
